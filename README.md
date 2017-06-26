07d2994e501265f033352e5ceb452f0fa8e09a5d
Ben Wiley <therealbenwiley@gmail.com>
2017-06-25 22:50:07 -0400

    Module can extract and save frames for a given source gif.

---
+++ README.md
@@ -0,0 +1,15 @@
+# gif-frames
+
+A pure JavaScript tool for extracting GIF frames and saving to file. Works in Node or the browser. Uses [get-pixels](https://github.com/scijs/get-pixels) and [save-pixels](https://github.com/scijs/save-pixels) under the hood.
+
+## require('gif-frames')(options)
+
+Options:
+* `input` (**required**): The pathname to the file, or an [in-memory Buffer](http://nodejs.org/api/buffer.html)
+* ***TODO:*** `frames` (**required**): The set of frames to extract (currently extracts all)
+* `filename` (*optional* if `input` is a pathname, **required** if `input` is a `Buffer`): The input file's name
+* `outputType` (*optional*, default "jpg"): Type to use for output (see [`type`](https://github.com/scijs/save-pixels#requiresave-pixelsarray-type-options) for `save-pixels`)
+* `quality` (*optional*): Jpeg quality (see [`quality`](https://github.com/scijs/save-pixels#requiresave-pixelsarray-type-options) for `save-pixels`)
+
+Returns:
+***TODO:*** A stream of the extracted frames
---
+++ gif-frames.js
@@ -0,0 +1,61 @@
+var path = require('path');
+var getPixels = require('get-pixels');
+var savePixels = require('save-pixels');
+var fs = require('fs');
+
+// https://stackoverflow.com/a/28203456
+function numDigits (x) {
+  return (Math.log10((x ^ (x >> 31)) - (x >> 31)) | 0) + 1;
+}
+
+// https://stackoverflow.com/a/2998822
+function padNumber (num, size) {
+  var s = num + '';
+  while (s.length < size) {
+    s = '0' + s;
+  }
+  return s;
+}
+
+function gifFrames (options, callback) {
+  options = options || {};
+  var input = options.input;
+  if (!input) {
+    callback(new Error('"input" option is required.'));
+    return;
+  }
+  var filename = options.filename
+    || (typeof input === 'string' ? path.basename(input) : '');
+  if (typeof input !== 'string' && !filename) {
+    callback(new Error('"filename" option is required for non-string inputs.'));
+    return;
+  }
+  var filenameType = path.extname(filename).slice(1);
+  var outputType = options.outputType || filenameType || 'jpg';
+  var outputPathBase = filename
+    && filenameType.toLowerCase() === outputType.toLowerCase()
+    && filename.slice(0, -(outputType.length + 1))
+    || filename;
+
+  getPixels(input, 'image/gif', function (err, pixels) {
+    if (err) {
+      callback(err);
+      return;
+    }
+    if (pixels.shape.length < 4) {
+      callback(new Error('Input should be multi-frame GIF.'));
+      return;
+    }
+    var frameCount = pixels.shape[0];
+    var frameCountDigits = numDigits(frameCount);
+    for (var i = 0; i < frameCount; i++) {
+      savePixels(pixels.pick(i), outputType, {
+        quality: options.quality
+      }).pipe(fs.createWriteStream(
+        outputPathBase + '-' + padNumber(i, frameCountDigits) + '.' + outputType
+      ));
+    }
+  });
+}
+
+module.exports = gifFrames;
