b8462404be39afa254b3c59e54ab5c871b6fa21e
Ben Wiley <therealbenwiley@gmail.com>
2017-06-26 02:22:41 -0400

    Module accepts data callback and returns promise; file writing taken out; streams exposed to user.

--- README.md
+++ README.md
@@ -10,15 +10,66 @@ npm install gif-frames
 
 ## `require('gif-frames')(options[, callback])`
 
-Options:
-* `input` (**required**): The pathname to the file, or an [in-memory Buffer](http://nodejs.org/api/buffer.html)
+### Options:
+
+* `url` (**required**): The pathname to the file, or an [in-memory Buffer](http://nodejs.org/api/buffer.html)
 * ***TODO:*** `frames` (**required**): The set of frames to extract (currently extracts all)
-* `filename` (*optional* if `input` is a pathname, **required** if `input` is a `Buffer`): The input file's name
 * `outputType` (*optional*, default "jpg"): Type to use for output (see [`type`](https://github.com/scijs/save-pixels#requiresave-pixelsarray-type-options) for `save-pixels`)
 * `quality` (*optional*): Jpeg quality (see [`quality`](https://github.com/scijs/save-pixels#requiresave-pixelsarray-type-options) for `save-pixels`)
 
-The optional callback is called if an error occurs, with the error object
-as its only argument.
+The callback accepts the arguments `(error, frameData)`.
 
-Returns:
-***TODO:*** A stream of the extracted frames
+### Returns:
+
+A `Promise` resolving to the `frameData` array (if promises are supported in the running environment)
+
+## `frameData`
+
+An array of objects of the form:
+
+```javascript
+{
+  getImageStream,
+  frameIndex
+}
+```
+
+## Examples
+
+Writing to file in Node:
+
+```javascript
+var gifFrames = require('gif-frames');
+var fs = require('fs');
+
+gifFrames({ url: 'image.gif', outputType: 'png' }, function (err, frameData) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  frameData.forEach(function (frame) {
+    frame.getImageStream().pipe(fs.createWriteStream(
+      'image-' + frame.frameIndex + '.png'
+    ));
+  });
+});
+```
+
+Writing to canvas in the browser (and using a `Promise`):
+
+```javascript
+var gifFrames = require('gif-frames');
+
+gifFrames({ url: 'image.gif', outputType: 'canvas' })
+  .then(function (frameData) {
+    frameData.forEach(function (frame) {
+      var canvas = document.createElement('canvas');
+      canvas.id = 'frame-' + frame.frameIndex;
+      document.body.appendChild(canvas);
+      frame.getImageStream().pipe(canvas);
+    });
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+```
--- gif-frames.js
+++ gif-frames.js
@@ -1,62 +1,79 @@
 var path = require('path');
 var getPixels = require('get-pixels');
 var savePixels = require('save-pixels');
-var fs = require('fs');
 
-// https://stackoverflow.com/a/28203456
-function numDigits (x) {
-  return (Math.log10((x ^ (x >> 31)) - (x >> 31)) | 0) + 1;
+function nopromises () {
+  throw new Error(
+    'Promises not supported in your environment. ' +
+    'Use the callback argument or a Promise polyfill.'
+  );
 }
 
-// https://stackoverflow.com/a/2998822
-function padNumber (num, size) {
-  var s = num + '';
-  while (s.length < size) {
-    s = '0' + s;
-  }
-  return s;
-}
+var brokenPromise = {
+  then: nopromises,
+  catch: nopromises
+};
 
 function gifFrames (options, callback) {
   options = options || {};
   callback = callback || function () {};
-  var input = options.input;
-  if (!input) {
-    callback(new Error('"input" option is required.'));
-    return;
-  }
-  var filename = options.filename
-    || (typeof input === 'string' ? path.basename(input) : '');
-  if (typeof input !== 'string' && !filename) {
-    callback(new Error('"filename" option is required for non-string inputs.'));
-    return;
-  }
-  var filenameType = path.extname(filename).slice(1);
-  var outputType = options.outputType || filenameType || 'jpg';
-  var outputPathBase = filename
-    && filenameType.toLowerCase() === outputType.toLowerCase()
-    && filename.slice(0, -(outputType.length + 1))
-    || filename;
 
-  getPixels(input, 'image/gif', function (err, pixels) {
+  var promise;
+  var resolve;
+  var reject;
+  if (typeof Promise === 'function') {
+    promise = new Promise(function (_resolve, _reject) {
+      resolve = function (res) {
+        callback(null, res);
+        _resolve(res);
+      };
+      reject = function (err) {
+        callback(err);
+        _reject(err);
+      };
+    });
+  } else {
+    promise = brokenPromise;
+    resolve = function (res) {
+      callback(null, res);
+    };
+    reject = callback;
+  }
+
+  var url = options.url;
+  if (!url) {
+    reject(new Error('"url" option is required.'));
+    return;
+  }
+  var outputType = options.outputType || 'jpg';
+  var quality = options.quality;
+
+  getPixels(url, 'image/gif', function (err, pixels) {
     if (err) {
-      callback(err);
+      reject(err);
       return;
     }
     if (pixels.shape.length < 4) {
-      callback(new Error('Input should be multi-frame GIF.'));
+      reject(new Error('"url" input should be multi-frame GIF.'));
       return;
     }
-    var frameCount = pixels.shape[0];
-    var frameCountDigits = numDigits(frameCount);
-    for (var i = 0; i < frameCount; i++) {
-      savePixels(pixels.pick(i), outputType, {
-        quality: options.quality
-      }).pipe(fs.createWriteStream(
-        outputPathBase + '-' + padNumber(i, frameCountDigits) + '.' + outputType
-      ));
+    var frameData = [];
+    for (var i = 0; i < pixels.shape[0]; i++) {
+      (function (frameIndex) {
+        frameData.push({
+          getImageStream: function () {
+            return savePixels(pixels.pick(frameIndex), outputType, {
+              quality: quality
+            });
+          },
+          frameIndex: frameIndex
+        });
+      })(i);
     }
+    resolve(frameData);
   });
+
+  return promise;
 }
 
 module.exports = gifFrames;
