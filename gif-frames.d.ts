e68f472745e08a418c3670fb457ee625d3e1d085
Tenpi <37512637+Tenpi@users.noreply.github.com>
2019-09-29 00:15:18 -0400

    Added typings

---
+++ gif-frames.d.ts
@@ -0,0 +1,47 @@
+import {Initializer} from "multi-integer-range";
+import {Canvas} from "canvas"
+
+declare module "gif-frames" {
+
+    export default function gifFrames<T extends GifFrameOptions>(options: T): Promise<GifFrameData<T>>;
+    export default function gifFrames<T extends GifFrameOptions>(options: T, callback: (err: Error, frameData: GifFrameData<T>) => void): void;
+
+    type GifOutputType = "jpeg" | "jpg" | "gif" | "png" | "canvas";
+    type GifFrameData<T extends GifFrameOptions> = T["outputType"] extends "canvas" ? GifFrameDataCanvas : GifFrameDataStream;
+
+    interface GifFrameOptions {
+        url: string | Buffer;
+        frame: "all" | Initializer;
+        outputType?: GifOutputType;
+        quality?: number;
+        cumulative?: boolean;
+    }
+
+    interface GifFrameDataCanvas {
+        getImage(): Canvas;
+        frameIndex: number;
+        frameInfo: GifFrameInfo
+    }
+
+    interface GifFrameDataStream {
+        getImage(): ReadableStream;
+        frameIndex: number;
+        frameInfo: GifFrameInfo
+    }
+
+    interface GifFrameInfo {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+        has_local_palette: boolean;
+        palette_offset: number;
+        palette_size: number;
+        data_offset: number;
+        data_length: number;
+        transparent_index: number;
+        interlaced: boolean;
+        delay: number;
+        disposal: number;
+    }
+}
